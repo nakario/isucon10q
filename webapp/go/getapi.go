257367cb3acb2838f806a01cbc408f9eedde5922
murata <murata@kmc.gr.jp>
2020-09-12 13:33:06 +0900

    コードを分離

---
+++ webapp/go/getapi.go
@@ -0,0 +1,379 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+// chair ////////////////////////////////////////////
+
+func getChairDetail(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Echo().Logger.Errorf("Request parameter \"id\" parse error : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	chair := Chair{}
+	query := `SELECT * FROM chair WHERE id = ?`
+	err = db.Get(&chair, query, id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Echo().Logger.Infof("requested id's chair not found : %v", id)
+			return c.NoContent(http.StatusNotFound)
+		}
+		c.Echo().Logger.Errorf("Failed to get the chair from id : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	} else if chair.Stock <= 0 {
+		c.Echo().Logger.Infof("requested id's chair is sold out : %v", id)
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.JSON(http.StatusOK, chair)
+}
+
+func searchChairs(c echo.Context) error {
+	conditions := make([]string, 0)
+	params := make([]interface{}, 0)
+
+	if c.QueryParam("priceRangeId") != "" {
+		chairPrice, err := getRange(chairSearchCondition.Price, c.QueryParam("priceRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("priceRangeID invalid, %v : %v", c.QueryParam("priceRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if chairPrice.Min != -1 {
+			conditions = append(conditions, "price >= ?")
+			params = append(params, chairPrice.Min)
+		}
+		if chairPrice.Max != -1 {
+			conditions = append(conditions, "price < ?")
+			params = append(params, chairPrice.Max)
+		}
+	}
+
+	if c.QueryParam("heightRangeId") != "" {
+		chairHeight, err := getRange(chairSearchCondition.Height, c.QueryParam("heightRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("heightRangeIf invalid, %v : %v", c.QueryParam("heightRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if chairHeight.Min != -1 {
+			conditions = append(conditions, "height >= ?")
+			params = append(params, chairHeight.Min)
+		}
+		if chairHeight.Max != -1 {
+			conditions = append(conditions, "height < ?")
+			params = append(params, chairHeight.Max)
+		}
+	}
+
+	if c.QueryParam("widthRangeId") != "" {
+		chairWidth, err := getRange(chairSearchCondition.Width, c.QueryParam("widthRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("widthRangeID invalid, %v : %v", c.QueryParam("widthRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if chairWidth.Min != -1 {
+			conditions = append(conditions, "width >= ?")
+			params = append(params, chairWidth.Min)
+		}
+		if chairWidth.Max != -1 {
+			conditions = append(conditions, "width < ?")
+			params = append(params, chairWidth.Max)
+		}
+	}
+
+	if c.QueryParam("depthRangeId") != "" {
+		chairDepth, err := getRange(chairSearchCondition.Depth, c.QueryParam("depthRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("depthRangeId invalid, %v : %v", c.QueryParam("depthRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if chairDepth.Min != -1 {
+			conditions = append(conditions, "depth >= ?")
+			params = append(params, chairDepth.Min)
+		}
+		if chairDepth.Max != -1 {
+			conditions = append(conditions, "depth < ?")
+			params = append(params, chairDepth.Max)
+		}
+	}
+
+	if c.QueryParam("kind") != "" {
+		conditions = append(conditions, "kind = ?")
+		params = append(params, c.QueryParam("kind"))
+	}
+
+	if c.QueryParam("color") != "" {
+		conditions = append(conditions, "color = ?")
+		params = append(params, c.QueryParam("color"))
+	}
+
+	if c.QueryParam("features") != "" {
+		for _, f := range strings.Split(c.QueryParam("features"), ",") {
+			conditions = append(conditions, "features LIKE CONCAT('%', ?, '%')")
+			params = append(params, f)
+		}
+	}
+
+	if len(conditions) == 0 {
+		c.Echo().Logger.Infof("Search condition not found")
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	conditions = append(conditions, "stock > 0")
+
+	page, err := strconv.Atoi(c.QueryParam("page"))
+	if err != nil {
+		c.Logger().Infof("Invalid format page parameter : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	perPage, err := strconv.Atoi(c.QueryParam("perPage"))
+	if err != nil {
+		c.Logger().Infof("Invalid format perPage parameter : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	searchQuery := "SELECT * FROM chair WHERE "
+	countQuery := "SELECT COUNT(*) FROM chair WHERE "
+	searchCondition := strings.Join(conditions, " AND ")
+	limitOffset := " ORDER BY popularity DESC, id ASC LIMIT ? OFFSET ?"
+
+	var res ChairSearchResponse
+	err = db.Get(&res.Count, countQuery+searchCondition, params...)
+	if err != nil {
+		c.Logger().Errorf("searchChairs DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	chairs := []Chair{}
+	params = append(params, perPage, page*perPage)
+	err = db.Select(&chairs, searchQuery+searchCondition+limitOffset, params...)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusOK, ChairSearchResponse{Count: 0, Chairs: []Chair{}})
+		}
+		c.Logger().Errorf("searchChairs DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	res.Chairs = chairs
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func getChairSearchCondition(c echo.Context) error {
+	return c.JSON(http.StatusOK, chairSearchCondition)
+}
+
+func getLowPricedChair(c echo.Context) error {
+	var chairs []Chair
+	query := `SELECT * FROM chair WHERE stock > 0 ORDER BY price ASC, id ASC LIMIT ?`
+	err := db.Select(&chairs, query, Limit)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Logger().Error("getLowPricedChair not found")
+			return c.JSON(http.StatusOK, ChairListResponse{[]Chair{}})
+		}
+		c.Logger().Errorf("getLowPricedChair DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, ChairListResponse{Chairs: chairs})
+}
+
+// estate ////////////////////////////////////////////
+
+func getEstateDetail(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Echo().Logger.Infof("Request parameter \"id\" parse error : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	var estate Estate
+	err = db.Get(&estate, "SELECT * FROM estate WHERE id = ?", id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Echo().Logger.Infof("getEstateDetail estate id %v not found", id)
+			return c.NoContent(http.StatusNotFound)
+		}
+		c.Echo().Logger.Errorf("Database Execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, estate)
+}
+
+func searchEstates(c echo.Context) error {
+	conditions := make([]string, 0)
+	params := make([]interface{}, 0)
+
+	if c.QueryParam("doorHeightRangeId") != "" {
+		doorHeight, err := getRange(estateSearchCondition.DoorHeight, c.QueryParam("doorHeightRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("doorHeightRangeID invalid, %v : %v", c.QueryParam("doorHeightRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if doorHeight.Min != -1 {
+			conditions = append(conditions, "door_height >= ?")
+			params = append(params, doorHeight.Min)
+		}
+		if doorHeight.Max != -1 {
+			conditions = append(conditions, "door_height < ?")
+			params = append(params, doorHeight.Max)
+		}
+	}
+
+	if c.QueryParam("doorWidthRangeId") != "" {
+		doorWidth, err := getRange(estateSearchCondition.DoorWidth, c.QueryParam("doorWidthRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("doorWidthRangeID invalid, %v : %v", c.QueryParam("doorWidthRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if doorWidth.Min != -1 {
+			conditions = append(conditions, "door_width >= ?")
+			params = append(params, doorWidth.Min)
+		}
+		if doorWidth.Max != -1 {
+			conditions = append(conditions, "door_width < ?")
+			params = append(params, doorWidth.Max)
+		}
+	}
+
+	if c.QueryParam("rentRangeId") != "" {
+		estateRent, err := getRange(estateSearchCondition.Rent, c.QueryParam("rentRangeId"))
+		if err != nil {
+			c.Echo().Logger.Infof("rentRangeID invalid, %v : %v", c.QueryParam("rentRangeId"), err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		if estateRent.Min != -1 {
+			conditions = append(conditions, "rent >= ?")
+			params = append(params, estateRent.Min)
+		}
+		if estateRent.Max != -1 {
+			conditions = append(conditions, "rent < ?")
+			params = append(params, estateRent.Max)
+		}
+	}
+
+	if c.QueryParam("features") != "" {
+		for _, f := range strings.Split(c.QueryParam("features"), ",") {
+			conditions = append(conditions, "features like concat('%', ?, '%')")
+			params = append(params, f)
+		}
+	}
+
+	if len(conditions) == 0 {
+		c.Echo().Logger.Infof("searchEstates search condition not found")
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	page, err := strconv.Atoi(c.QueryParam("page"))
+	if err != nil {
+		c.Logger().Infof("Invalid format page parameter : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	perPage, err := strconv.Atoi(c.QueryParam("perPage"))
+	if err != nil {
+		c.Logger().Infof("Invalid format perPage parameter : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	searchQuery := "SELECT * FROM estate WHERE "
+	countQuery := "SELECT COUNT(*) FROM estate WHERE "
+	searchCondition := strings.Join(conditions, " AND ")
+	limitOffset := " ORDER BY popularity DESC, id ASC LIMIT ? OFFSET ?"
+
+	var res EstateSearchResponse
+	err = db.Get(&res.Count, countQuery+searchCondition, params...)
+	if err != nil {
+		c.Logger().Errorf("searchEstates DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	estates := []Estate{}
+	params = append(params, perPage, page*perPage)
+	err = db.Select(&estates, searchQuery+searchCondition+limitOffset, params...)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusOK, EstateSearchResponse{Count: 0, Estates: []Estate{}})
+		}
+		c.Logger().Errorf("searchEstates DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	res.Estates = estates
+
+	return c.JSON(http.StatusOK, res)
+}
+
+func getLowPricedEstate(c echo.Context) error {
+	estates := make([]Estate, 0, Limit)
+	query := `SELECT * FROM estate ORDER BY rent ASC, id ASC LIMIT ?`
+	err := db.Select(&estates, query, Limit)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Logger().Error("getLowPricedEstate not found")
+			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
+		}
+		c.Logger().Errorf("getLowPricedEstate DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, EstateListResponse{Estates: estates})
+}
+
+func searchRecommendedEstateWithChair(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.Logger().Infof("Invalid format searchRecommendedEstateWithChair id : %v", err)
+		return c.NoContent(http.StatusBadRequest)
+	}
+
+	chair := Chair{}
+	query := `SELECT * FROM chair WHERE id = ?`
+	err = db.Get(&chair, query, id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.Logger().Infof("Requested chair id \"%v\" not found", id)
+			return c.NoContent(http.StatusBadRequest)
+		}
+		c.Logger().Errorf("Database execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	var estates []Estate
+	w := chair.Width
+	h := chair.Height
+	d := chair.Depth
+	query = `SELECT * FROM estate WHERE (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) ORDER BY popularity DESC, id ASC LIMIT ?`
+	err = db.Select(&estates, query, w, h, w, d, h, w, h, d, d, w, d, h, Limit)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
+		}
+		c.Logger().Errorf("Database execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, EstateListResponse{Estates: estates})
+}
+
+func getEstateSearchCondition(c echo.Context) error {
+	return c.JSON(http.StatusOK, estateSearchCondition)
+}
