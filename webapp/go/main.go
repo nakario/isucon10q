6819417a4553ab1899c924d1101eb61fae5ea00c
isucon <isucon@example.com>
2020-09-18 18:26:59 +0000

    log off go

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -13,7 +13,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	_ "github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
 )
 
@@ -123,12 +123,14 @@ func initialize(c echo.Context) error {
 func main() {
 	// Echo instance
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	e.Debug = false
+	e.Logger.SetLevel(log.OFF)
+	// e.Debug = true
+	// e.Logger.SetLevel(log.DEBUG)
 
 	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
 
 	// Initialize
 	e.POST("/initialize", initialize)
